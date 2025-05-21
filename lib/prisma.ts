9ef4bee5275fcc63336803866b7e02a8609a1755
Your Name <sanjayshreeram77@gmail.com>
2025-05-21 20:49:50 +0530

    added changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,18 +11,4 @@ export const prisma =
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 // Keep Alive function to ping DB every 4 minutes
-function keepAlive() {
-  setInterval(async () => {
-    try {
-      await prisma.$queryRaw`SELECT 1`
-      console.log('DB connection alive ping sent')
-    } catch (err) {
-      console.error('Failed keep-alive ping:', err)
-    }
-  }, 4 * 60 * 1000)
-}
 
-// Run keepAlive only in server environment (not during build)
-if (typeof window === 'undefined') {
-  keepAlive()
-}
